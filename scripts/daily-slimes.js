6874f148a1d1f81919d01bb711af0abad3cbe6af
EPmaple <tcen17@bu.edu>
2023-07-22 09:48:08 -0400

    python timestamps to ISO format to JS date objects

--- scripts/daily-slimes.js
+++ scripts/daily-slimes.js
@@ -10,6 +10,22 @@ document.getElementById("js_dailies_slider").style.display = "block"
 
 // console.log(typeof slime_records) slime_records is a list of objects
 
+/**************************************/
+/**************************************/
+
+// Assuming the original timestamp format is "YYYY-MM-DD HH:mm:ss.ssssss"
+function convertToISOFormat(timestamp) {
+  const parts = timestamp.split(" ");
+  const datePart = parts[0];
+  const timePart = parts[1];
+  const milliseconds = parseFloat(`0.${timePart.split('.')[1]}`).toFixed(2).substr(2); // Rounding to 3 decimal places and removing the leading "0."
+  const isoTimestamp = `${datePart}T${timePart.split('.')[0]}.${milliseconds}Z`;
+  return isoTimestamp;
+}
+
+/**************************************/
+/**************************************/
+
 let timestamps = []
 // Extract timestamps and number of slimes
 for (let i = 0; i < slime_records.length; i++) {
@@ -17,52 +33,65 @@ for (let i = 0; i < slime_records.length; i++) {
   timestamps.push(time)
 }
 
+// Convert each timestamp in the array to ISO format
+timestamps = timestamps.map(convertToISOFormat); // ex.) '2023-07-21T23:21:49.99Z'
+
+// Use moment.js to parse the timestamps and convert them to JavaScript Date objects
+const dateObjects = timestamps.map((timestamp) => moment(timestamp).toDate())
+
 // Generate an array with a value of 1 for each timestamp (since there's one slime per timestamp)
 const slimeCounts = Array.from({length: timestamps.length}, () => 1)
 //console.log(slimeCounts)
 
+
 function dailiesChart() {
-  const data = {
-    labels: timestamps,
-    datasets: [
-      {
-        label: 'Number of Slimes',
-        data: slimeCounts,
-        borderColor: 'rgba(255, 99, 132, 1)',
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderWidth: 2,
-        fill: false,
-      }
-    ]
-  }
-  
-  const options = {
-    scales: {
-      x: {
-        type: 'time',
-        time: {
-          unit: 'hour',
+  try {
+    const data = {
+      labels: dateObjects,
+      datasets: [
+        {
+          label: 'Number of Slimes',
+          data: slimeCounts,
+          borderColor: 'rgba(255, 99, 132, 1)',
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderWidth: 2,
+          fill: false,
+        }
+      ]
+    }
+    
+    const options = {
+      scales: {
+        x: {
+          type: 'time',
+          time: {
+            unit: 'hour',
+          }
+        },
+        y: {
+          beginAtZero: true,
+          stepSize: 1,
+          type: 'linear',
         }
       },
-      y: {
-        beginAtZero: true,
-        stepSize: 1,
-        type: 'linear',
-      }
-    },
-    responsive: true,
-    maintainAspectRatio: false,
+      responsive: true,
+      maintainAspectRatio: false,
+    }
+    
+    const config = {
+      type: "line",
+      data,
+      options
+    }
+  
+    const ctx = document.getElementById("js_dailies_chart").getContext("2d")
+  
+    const chart = new Chart(ctx, config)
+
+  } catch (error) {
+    console.error("An error occurred: ", error.message)
   }
   
-  const config = {
-    type: "line",
-    data,
-    options
-  }
-
-  const ctx = document.getElementById("js_dailies_chart").getContext("2d")
-
-  const chart = new Chart(ctx, config)
   
   /*
   const chart = new Chart(
